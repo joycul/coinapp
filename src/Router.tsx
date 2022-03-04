46ea8236732d9eb7ad744fb44fe4309f5e2792a7
joycul <94051546+joycul@users.noreply.github.com>
2022-03-05 03:09:21 +0900

    homepage

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ function Router() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/:coinId">
+        <Route path="/coinapp/:coinId">
           <Coin />
         </Route>
         <Route path="/">
